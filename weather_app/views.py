a89b4f1aa2de74c432d0d2da74a3c5fd08c154d4
kalosius <kalosius43@gmail.com>
2023-10-28 23:26:54 +0300

    final tempate lay out

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
-import requests   
+import requests
 import datetime
 
 # Create your views here.
-def index(request): 
-
-    if 'city' in requests.POST:
+def index(request):
+    if 'city' in request.POST:
         city = request.POST['city']
     else:
-        city = 'indore'
+        city = 'kampala'
 
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=607653a33a21bafbf41f4b4392c50e71'
-    PARAMS = {'units':'metric'}
+    PARAMS = {'units': 'metric'}
 
-    data = request.get(url, PARAMS).json()
+    response = requests.get(url, params=PARAMS)
 
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
+    if response.status_code == 200:
+        data = response.json()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+    else:
+        description = 'Error fetching data'
+        icon = ''
+        temp = ''
 
     day = datetime.date.today()
 
-    return render(request, 'weatherapp/index.html', {'description':description, 'icon':icon, 'temp':temp, 'day':day})
+    return render(request, 'weatherapp/index.html', {'description': description, 'icon': icon, 'temp': temp, 'day': day, 'city':city})
